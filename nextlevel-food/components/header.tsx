3b1bc0fcaefa42025c98d42d0db5ef4904599b00
Mari Spinola <89092249+Mapinko@users.noreply.github.com>
2024-03-21 11:10:07 -0300

    delete: unnused files

--- nextlevel-food/components/header.tsx
+++ nextlevel-food/components/header.tsx
@@ -3,8 +3,7 @@ import Image from 'next/image';
 const Header = ()   => {
     return (
         <header>
-            <Image src="/logo.png" alt="A server surrounded by magic sparkles." width={500} height={500}/>
-            <h1>Next Level Food</h1>
+
         </header>
     )
 }

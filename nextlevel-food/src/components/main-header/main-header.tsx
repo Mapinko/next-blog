3993ffb4bc0d52ca824858e7b6aa22e042bc7db1
Mari Spinola <89092249+Mapinko@users.noreply.github.com>
2024-03-21 14:20:40 -0300

    fix: main header background

--- nextlevel-food/src/components/main-header/main-header.tsx
+++ nextlevel-food/src/components/main-header/main-header.tsx
@@ -3,26 +3,32 @@ import classes from './main-header.module.css';
 import logoImg from '../../assets/logo.png';
 import Image from 'next/image';
 
+import MainHeaderBackground from './main-header-background';
+
 const MainHeader = ()   => {
     return (
-        <header className={classes.header}>
-            <Link className={classes.logo} href="/">
-                    <Image src={logoImg} alt="A plate with food on it" priority/>
-                    NextLevel Food
-            </Link>
+        <>
+            <MainHeaderBackground/>
 
-            <nav className={classes.nav}>
-                <ul>
-                    <li>
-                        <Link href="/meals">Browse Meals</Link>
-                    </li>
+            <header className={classes.header}>
+                <Link className={classes.logo} href="/">
+                        <Image src={logoImg} alt="A plate with food on it" priority/>
+                        NextLevel Food
+                </Link>
 
-                    <li>
-                        <Link href="/community">Foodies Community</Link>
-                    </li>
-                </ul>
-            </nav>
-        </header>
+                <nav className={classes.nav}>
+                    <ul>
+                        <li>
+                            <Link href="/meals">Browse Meals</Link>
+                        </li>
+
+                        <li>
+                            <Link href="/community">Foodies Community</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+        </>
     )
 }
 
